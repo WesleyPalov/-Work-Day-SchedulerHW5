e3141c666487587aca7946fbf7a609ad0723887e
WesleyCod <wesleypalov@yahoo.com>
2023-02-18 22:50:11 -0700

    partally created local storage

--- script.js
+++ script.js
@@ -1,6 +1,54 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+// fun
+var today = dayjs();
+var dayWeek =  today.format('dddd, HH:mm');
+
+var currentTime = today.format('HH');
+var test = document.getElementById("hour-12"); 
+
+var saveBtn = document.querySelector(".container-lg");
+var nineAm = $("#9AM");
+var tenAm = $("10AM");
+
+
+// getting info from local storage
+function initPage() {
+
+ 
+  var init9 = JSON.parse(localStorage.getItem("9AM"));
+  nineAm.val(init9);
+
+
+  console.log(nineAm);
+ // var init10 = JSON.parse(localStorage.getItem("10:00 am"))
+  //tenAm.val(init10);
+};
+
+
+
+
+
+
+  // Buttons (save to Local Storage)
+  $(".saveBtn").on("click", function(){
+ // console.log(test);
+  userInput = $(this).siblings(".description").val().trim();
+  //console.log(userInput);
+  textArea = $(this).siblings("").text().trim();
+  console.log(textArea);
+
+  localStorage.setItem(textArea, JSON.stringify(userInput));
+  })
+
+//Checking time to determine present, past, or future
+//$('.time-block').each(function() {
+ // $(this).find('.text-area').val(availableHours[$(this).attr('data-time')].value);
+//});
+
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
